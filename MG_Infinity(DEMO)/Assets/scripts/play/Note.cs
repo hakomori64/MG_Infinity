cb430e02130722049b57c761b2e66b490effcbb8
hakomori64 <hakomori64@gmail.com>
2018-11-08 00:42:46 +0900

    edit NoteController.cs and Note.cs

---
+++ MG_Infinity(DEMO)/Assets/scripts/play/Note.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+	private string route;
+	private int id;
+	private double start, end;
+	private float radius, speed;
+	private float time = 0;
+	private bool head;
+	private int kindOfNote;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (head) {
+
+		} else {
+
+		}
+		time += Time.deltaTime;
+	}
+
+	public void Create (string route, int id, double start, double end, float radius, float speed, int kindOfNote, bool head) {
+		this.route = route;
+		this.id = id;
+		this.start = start;
+		this.end = end;
+		this.radius = radius;
+		this.speed = speed;
+		this.kindOfNote = kindOfNote;
+		this.head = head;
+	}
+}
--- MG_Infinity(DEMO)/Assets/scripts/play/NoteController.cs
+++ MG_Infinity(DEMO)/Assets/scripts/play/NoteController.cs
@@ -6,20 +6,46 @@ using UnityEngine;
 // this class administrate note score, time, instantiating and destroying.
 public class NoteController : MonoBehaviour {
 
+	enum KindsOfNote {
+		HitNotes,
+		LongNotes,
+		SwipeNotes
+	}
+
 	public int id;
 	private string route;
 	private double start, end;
 	private float radius, speed;
+	private KindsOfNote kindOfNote;
 
 	private float time = 0;
-	private GameObject notePrefab;
+	public GameObject notePrefab;
+	private GameObject[] notes;
 	void Start () {
-		analyzeRoute();
+		detectKindsOfNote();
+		
 		//notePrefab = (GameObject)Resources.Load("prefabs/note");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (time == 0) {
+			notes[0].SetActive(true);
+		}
+
+		if (this.kindOfNote != KindsOfNote.HitNotes) {
+			if (time >= this.end - this.start && time < (this.end - this.start) + Time.deltaTime) {
+				notes[1].SetActive(true);
+			}
+		}
+
+		if (time >= this.end - this.start + this.radius / this.speed) {
+			this.notes[0].SetActive(false);
+			if (this.notes.Length == 2) {
+				this.notes[1].SetActive(false);
+			}
+			this.gameObject.SetActive(false);
+		}
 		
 		time += Time.deltaTime;
 	}
@@ -30,10 +56,33 @@ public class NoteController : MonoBehaviour {
 		this.end = end;
 		this.radius = radius;
 		this.speed = speed;
+		Debug.Log((int)KindsOfNote.HitNotes);
+		Debug.Log((int)KindsOfNote.LongNotes);
+		Debug.Log((int)KindsOfNote.SwipeNotes);
 	}
 
 	//
-	void analyzeRoute() {
-
+	void detectKindsOfNote() {
+		if (Mathf.Abs((float)(this.end - this.start)) < Mathf.Epsilon) { //hitnotes
+			this.kindOfNote = KindsOfNote.HitNotes;
+			this.notes = new GameObject[1];
+			this.notes[0] = Instantiate(notePrefab, transform.position, transform.rotation);
+			Note _note = this.notes[0].GetComponent<Note>();
+			_note.Create(this.route, this.id, this.start, this.end, this.radius, this.speed, (int)this.kindOfNote, true);
+		} else { //long | swipe note
+			this.notes = new GameObject[2];
+			this.notes[0] = Instantiate(notePrefab, transform.position, transform.rotation);
+			Note _note = this.notes[0].GetComponent<Note>();
+			_note.Create(this.route, this.id, this.start, this.end, this.radius, this.speed, (int)kindOfNote, true);
+			this.notes[1] = Instantiate(notePrefab, transform.position, transform.rotation);
+			_note = this.notes[1].GetComponent<Note>();
+			_note.Create(this.route, this.id, this.start, this.end, this.radius, this.speed, (int)this.kindOfNote, false);
+			
+			if (this.route.Length == 1) {
+				this.kindOfNote = KindsOfNote.LongNotes;
+			} else {
+				this.kindOfNote = KindsOfNote.SwipeNotes;
+			}
+		}
 	}
 }

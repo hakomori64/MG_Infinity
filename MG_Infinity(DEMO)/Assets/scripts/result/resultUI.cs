976173609994d35431b3a9c7ec3b798872dcb394
syun1015 <syun1015@gmail.com>
2019-02-07 11:41:34 +0900

    result画面を作っています/現状ではスコアの受け渡しに失敗しているThu Feb  7 11:40:56 JST 2019

---
+++ MG_Infinity(DEMO)/Assets/scripts/result/resultUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; 
+public class resultUI : MonoBehaviour {
+
+	private int ceilingScore = 100000;
+	private int scoreSum = 0;
+	public ScoreCount finalScore = ScoreCount();
+	
+	public GameObject score_object = null;
+	// Use this for initialization
+	void Start () {
+		if (Application.isEditor) {
+			finalScore.bad = 0;
+			finalScore.good = 0;
+			finalScore.great = 0;
+			finalScore.perfect = 0;
+		} else { 
+			finalScore = GameController.GetScoreCount();
+
+		}
+		
+		Text scoreText = score_object.GetComponent<Text> ();
+		scoreText.text = String.Format("{0:D6}", scoreSum);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
